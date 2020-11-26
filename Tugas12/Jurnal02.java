19a4eb287ed64d7a6ba7eb2e146f522af14eac8b
Lucky7tb <Luckytri101@gmail.com>
2020-11-27 00:27:28 +0700

    can add tictactoe size

--- Tugas12/Jurnal02.java
+++ Tugas12/Jurnal02.java
@@ -5,8 +5,9 @@ class Jurnal02{
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
 
-		int[][] ticTacToe = new int[3][3];
-		int oCounter = 0,
+		int[][] ticTacToe;
+		int ticTacToeSize,
+			oCounter = 0,
 			xCounter = 0;
 		char character = '-';
 		boolean win = false;
@@ -16,9 +17,14 @@ class Jurnal02{
 		System.out.println("Kelas : D3-RPL-4401");    
 		System.out.println("NIM   : 6706202055\n");
 
+		System.out.print("Masukan ukuran ticTacToe: ");
+		ticTacToeSize = userInput.nextInt();
+
+		ticTacToe = new int[ticTacToeSize][ticTacToeSize];
+
 		System.out.println("Silahkan angka tic tac toe: ");
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
+		for(int i = 0; i < ticTacToeSize; i++){
+			for(int j = 0; j < ticTacToeSize; j++){
 				ticTacToe[i][j] = userInput.nextInt();
 			}
 		}
@@ -28,11 +34,11 @@ class Jurnal02{
 		System.out.print("\n");
 
 		// Cek Horizontal
-		for(int i = 0; i < 3; i++){
-			for(int k = 0; k < 3; k++){
+		for(int i = 0; i < ticTacToeSize; i++){
+			for(int k = 0; k < ticTacToeSize; k++){
 				if(ticTacToe[i][k] == 1){
 					oCounter++;
-					if(oCounter == 3){
+					if(oCounter == ticTacToeSize){
 						character = 'o';
 						oCounter = 0;
 						win = true;
@@ -40,7 +46,7 @@ class Jurnal02{
 					}
 				}else if(ticTacToe[i][k] == 2){
 					xCounter++;
-					if(xCounter == 3){
+					if(xCounter == ticTacToeSize){
 						character = 'x';
 						xCounter = 0;
 						win = true;
@@ -54,11 +60,11 @@ class Jurnal02{
 
 		if(!win){
 			//Cek Vertical
-			for(int l = 0; l < 3; l++){
-				for(int m = 0; m < 3; m++){
+			for(int l = 0; l < ticTacToeSize; l++){
+				for(int m = 0; m < ticTacToeSize; m++){
 					if(ticTacToe[m][l] == 1){
 						oCounter++;
-						if(oCounter == 3){
+						if(oCounter == ticTacToeSize){
 							character = 'o';
 							oCounter = 0;
 							win = true;
@@ -66,7 +72,7 @@ class Jurnal02{
 						}
 					}else if(ticTacToe[m][l] == 2){
 						xCounter++;
-						if(xCounter == 3){
+						if(xCounter == ticTacToeSize){
 							character = 'x';
 							xCounter = 0;
 							win = true;
@@ -82,12 +88,12 @@ class Jurnal02{
 
 		if(!win){
 			//Cek Diagonal kiri dan kanan
-			int maxSize = 2; 
-			for(int l = 0; l < 3; l++){
+			int maxSize = ticTacToeSize - 1; 
+			for(int l = 0; l < ticTacToeSize; l++){
 				//Cek diagonal kiri ke kanan
 				if(ticTacToe[l][l] == 1){
 					++oCounter;
-					if(oCounter == 3){
+					if(oCounter == ticTacToeSize){
 						character = 'o';
 						oCounter = 0;
 						win = true;
@@ -95,7 +101,7 @@ class Jurnal02{
 					}
 				}else if(ticTacToe[l][l] == 2){
 					++xCounter;
-					if(xCounter == 3){
+					if(xCounter == ticTacToeSize){
 						character = 'x';
 						xCounter = 0;
 						win = true;
@@ -106,7 +112,7 @@ class Jurnal02{
 				//Cek diagonal kanan ke kiri
 				if(ticTacToe[l][maxSize] == 1){
 					++oCounter;
-					if(oCounter == 3){
+					if(oCounter == ticTacToeSize){
 						character = 'o';
 						oCounter = 0;
 						win = true;
@@ -114,7 +120,7 @@ class Jurnal02{
 					}
 				}else if(ticTacToe[l][maxSize] == 2){
 					++xCounter;
-					if(xCounter == 3){
+					if(xCounter == ticTacToeSize){
 						character = 'x';
 						xCounter = 0;
 						win = true;
@@ -126,7 +132,7 @@ class Jurnal02{
 		}
 
 		if(!win){
-			for(int i = 0; i < 3; i++){
+			for(int i = 0; i < ticTacToeSize; i++){
 				for(int num : ticTacToe[i]){
 					if(num == 0){
 						character = '?';
